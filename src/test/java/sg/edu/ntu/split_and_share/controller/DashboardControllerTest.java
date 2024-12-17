089d539646e8d4232d6526b26c75bf4351faabb5
Johnny Lee <cloudi32003@gmail.com>
2024-12-17 21:35:11 +0800

    Dashboard Controller with 3 Test Error v3

--- src/test/java/sg/edu/ntu/split_and_share/controller/DashboardControllerTest.java
+++ src/test/java/sg/edu/ntu/split_and_share/controller/DashboardControllerTest.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
